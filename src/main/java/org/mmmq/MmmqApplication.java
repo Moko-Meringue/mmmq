1d4e797063e78fca6b3defd29649adb348a479ee
KimDaehyeon <daehyeon3351@gmail.com>
2025-10-08 18:15:06 +0900

    Initial Commit

---
+++ src/main/java/org/mmmq/MmmqApplication.java
@@ -0,0 +1,13 @@
+package org.mmmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmmqApplication.class, args);
+	}
+
+}
