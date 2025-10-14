a2352b242594467c45677fe9aa08118a25163040
KimDaehyeon <daehyeon3351@gmail.com>
2025-10-14 22:50:19 +0900

    chore: auto configuration 활성화
    Co-authored-by: songsunkook <songsunkook@gmail.com>

---
+++ src/main/java/org/mmmq/MmmqAutoConfiguration.java
@@ -0,0 +1,10 @@
+package org.mmmq;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "org.mmmq")
+public class MmmqAutoConfiguration {
+
+}
