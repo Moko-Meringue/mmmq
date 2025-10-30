24e2602ec578ea40740dafc961632b4d15cbd10c
songsunkook <songsunkook@gmail.com>
2025-10-30 21:30:56 +0900

    test: Subscriber 빈 등록 테스트
    Co-authored-by: cookie-meringue <daehyeon3351@gmail.com>

---
+++ src/test/java/org/mmmq/core/config/SubscriberConfigurationTest.java
@@ -0,0 +1,45 @@
+package org.mmmq.core.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mmmq.core.TestConfiguration;
+import org.mmmq.core.fixture.HostFixture;
+import org.mmmq.core.subscriber.Subscriber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    classes = {TestConfiguration.class, SubscriberConfigurationTest.SubscriberConfiguration.class}
+)
+public class SubscriberConfigurationTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    @DisplayName("Subscriber 빈이 정상적으로 생성된다.")
+    void subscriberBeanCreationTest() {
+        Map<String, Subscriber> beans = applicationContext.getBeansOfType(Subscriber.class);
+
+        assertThat(beans.keySet()).containsExactly("testSubscriber");
+    }
+
+    @Configuration
+    static class SubscriberConfiguration {
+
+        @Bean("testSubscriber")
+        public Subscriber subscriber() {
+            return new Subscriber.Builder("name", HostFixture.localhost())
+                .subscribes("topic1", "topic2")
+                .build();
+        }
+    }
+}
