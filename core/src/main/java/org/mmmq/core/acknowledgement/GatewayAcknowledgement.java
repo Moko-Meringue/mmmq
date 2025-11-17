bb29d775d2d50556c1b8305dde642697c18c90be
songsunkook <songsunkook@gmail.com>
2025-11-17 19:32:05 +0900

    feat: ACK 확인 메서드 추가
    Co-authored-by: cookie-meringue <daehyeon3351@gmail.com>

--- core/src/main/java/org/mmmq/core/acknowledgement/GatewayAcknowledgement.java
+++ core/src/main/java/org/mmmq/core/acknowledgement/GatewayAcknowledgement.java
@@ -3,4 +3,8 @@ package org.mmmq.core.acknowledgement;
 public record GatewayAcknowledgement(
         Acknowledgement acknowledgement
 ) {
+
+    public boolean isAck() {
+        return this.acknowledgement == Acknowledgement.ACK;
+    }
 }
